7099f0993907fae299d157e17e72a110fb5d6b59
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-12 14:27:07 +0100

    admin check

--- actions/update-NIF-data.ts
+++ actions/update-NIF-data.ts
@@ -1,13 +1,21 @@
 "use server";
 import { db } from "@/lib/db";
 import { NIFSchema } from "@/schemas";
-import { parse } from "csv-parse";
-import formidable, { errors as formidableErrors } from "formidable";
 
 import { z } from "zod";
 import { revalidateTag } from "next/cache";
+import { currentUser } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
 
 export const updateNIFData = async (data: z.infer<typeof NIFSchema>[]) => {
+  const user = await currentUser();
+
+  if (!user) {
+    return { error: "Du er ikke logget inn" };
+  } else if (user.role !== UserRole.ADMIN) {
+    return { error: "Du er ikke administrator" };
+  }
+
   let rowsAdded = 0;
   let rowsUpdated = 0;
   try {
