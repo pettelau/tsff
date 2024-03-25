eaab35c2dc3d189755e2b17df3ac6db08c3a5e47
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-25 16:17:18 +0100

    simplified logic

--- app/(public)/serie/stats/page.tsx
+++ app/(public)/serie/stats/page.tsx
@@ -1,91 +1,40 @@
-import { GoalTable } from "@/app/components/stats/goalTable";
-import { YellowTable } from "@/app/components/stats/yellowTable";
-import { RedTable } from "@/app/components/stats/redTable";
-import { getGoalStats } from "@/data/getGoalStats";
-import { getYellowStats } from "@/data/getYellowStats";
-import { getRedStats } from "@/data/getRedStats";
+import { MatchEventType } from "@prisma/client";
+import {
+  PlayerMatchEventStats,
+  getMatchEventStats,
+} from "@/data/getMatchEventStats";
 import { getPlayersWithClubs } from "@/data/getPlayersWithClubs";
+import { StatsTable } from "@/app/components/stats/StatsTable";
+import { PlayerStats } from "@/types/types";
+import { matchEventTypesList } from "@/lib/enum-mappings";
 
 const StatsPage = async () => {
-  // Henter spillernavn og klubbinformasjon
   const playersWithClubs = await getPlayersWithClubs();
-  // Henter målstatistikk
-  const playerStats = await getGoalStats();
 
-  const goalStats = playersWithClubs
-    .map((playerWithClub) => {
-      const clubInfo = playerWithClub.club
-        ? playerWithClub.club
-        : { id: 0, name: "-" };
-      const goals =
-        playerStats.find((stat) => stat.player.id === playerWithClub.id)
-          ?.goals ?? 0;
-      return {
-        id: playerWithClub.id,
-        firstName: playerWithClub.firstName,
-        lastName: playerWithClub.lastName ?? "",
-        clubName: clubInfo.name ?? "",
-        goals,
-      };
-    })
-    .sort((a, b) => b.goals - a.goals)
-    .slice(0, 10);
-
-  // Henter gule kort
-  const yellowStats = await getYellowStats();
-
-  const yellowsStats = playersWithClubs
-    .map((playerWithClub) => {
-      const clubInfo = playerWithClub.club
-        ? playerWithClub.club
-        : { id: 0, name: "-" };
-      const yellows =
-        yellowStats.find((stat) => stat.player.id === playerWithClub.id)
-          ?.yellows ?? 0;
-      return {
-        id: playerWithClub.id,
-        firstName: playerWithClub.firstName,
-        lastName: playerWithClub.lastName ?? "",
-        clubName: clubInfo.name ?? "",
-        yellows,
-      };
-    })
-    .sort((a, b) => b.yellows - a.yellows) 
-    .slice(0, 10);
-
-  // Henter røde kort
-  const redStats = await getRedStats();
-
-  const redsStats = playersWithClubs
-    .map((playerWithClub) => {
-      const clubInfo = playerWithClub.club
-        ? playerWithClub.club
-        : { id: 0, name: "-" };
-      const reds =
-        redStats.find((stat) => stat.player.id === playerWithClub.id)
-          ?.reds ?? 0;
-      return {
-        id: playerWithClub.id,
-        firstName: playerWithClub.firstName,
-        lastName: playerWithClub.lastName ?? "",
-        clubName: clubInfo.name ?? "",
-        reds,
-      };
-    })
-    .sort((a, b) => b.reds - a.reds)
-    .slice(0, 10);
+  const statsData = await Promise.all(
+    matchEventTypesList.map(async ({ type }) => {
+      const eventType = type as MatchEventType;
+      const eventData = await getMatchEventStats(eventType);
+      return { tableData: eventData, eventType: eventType };
+    }),
+  );
 
   return (
-    <div className="flex flex-col items-center my-20">
-      <div className="bg-primary rounded-xl text-center p-3 text-white mb-4">
-        <GoalTable tableData={goalStats} name="Toppscorer" />
-      </div>
-      <div className="bg-primary rounded-xl text-center p-3 text-white mb-4">
-        <YellowTable tableData={yellowsStats} name="Gule Kort" />
-      </div>
-      <div className="bg-primary rounded-xl text-center p-3 text-white mb-4">
-        <RedTable tableData={redsStats} name="Røde Kort" />
-      </div>
+    <div className="flex flex-col px-4 sm:px-6 justify-center items-center space-y-4">
+      {statsData.map(
+        (
+          {
+            tableData,
+            eventType,
+          }: {
+            tableData: PlayerMatchEventStats[];
+            eventType: MatchEventType;
+          },
+          index: number,
+        ) => (
+          <StatsTable key={index} tableData={tableData} event={eventType} />
+        ),
+      )}
     </div>
   );
 };
